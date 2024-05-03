9e3aaa8e590c1c548ee5741207769e074e22f6d9
gideon <gideonmunene0@gmail.com>
2024-05-03 09:19:05 -0400

    event handling

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,17 @@
-// Code Keypad Component Here
 
-function Keypad (){
+// Code Keypad Component Here
+import React from "react";
+
+function Keypad() {
+
+    function handlePasswordChange() {
+        console.log("Entering password...");
+    }
     return (
-        <div></div>
-    )
+
+        <input type="password" onChange={handlePasswordChange} />
+
+    );
 }
 
 export default Keypad;
